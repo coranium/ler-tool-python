7c0fa160e3c34ed8312517f3820b808979a7b6d8
coranium <coranium_tyy@hotmail.com>
2021-05-04 18:23:48 +0800

    first commit

---
+++ LER_script.py
@@ -0,0 +1,29 @@
+
+
+import cv2 #(OpenCV3)
+from LER import edge_roughness
+from matplotlib import pyplot as plt
+import matplotlib.ticker as ticker
+
+
+image = cv2.imread("test.jpg")
+
+LER = edge_roughness()
+(Xcln, Ycln, freq, FourierPow) = LER.LER_analysis(image, 50e-6, 2) #image, image width (m), threshold for outliers
+
+
+plt.plot(Xcln, Ycln, '-')
+plt.plot(Xcln, Ycln, 'r.')
+plt.show()
+
+plt.plot(freq[2:100], FourierPow[2:100],'-')
+ax = plt.gca()
+ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0e'))
+plt.xlabel('cycles (m$^{-1}$)')
+plt.ylabel('Fourier power (au)')
+plt.title('Fourier power spectrum')
+plt.show() 
+
+cv2.imshow('Origional Image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ README.md
@@ -0,0 +1,1 @@
+#LER Tool in python
